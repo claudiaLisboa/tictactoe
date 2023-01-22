0c89a60c648fe0576bdad4f777312eda5ea69585
Claudia Lisboa <ccalumby@gmail.com>
2023-01-22 00:09:31 -0500

    FEAT: update function TicTacToe

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,13 +1,30 @@
 
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import './TicTacToe.css';
 
 function TicTacToe() {
-  return (
-    <div className="">
-    
-    </div>
-  );
+  const emptyBoard = Array(9).fill("");
+
+  const [board, setBoard] = useState(emptyBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("O");
+  const [winner, setWinner] = useState(null);
+
+  const handleCellClick = (index)=>{
+    if(winner){
+      console.log("Fim de Jogo!");
+      return null;
+    } 
+
+    if(board[index] !== ""){
+      console.log("Posição ocupada!");
+      return null;
+    } 
+
+    setBoard(board.map((item, itemIndex) => itemIndex ===index ? currentPlayer : item));
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  }
+
+
 }
 
 export default TicTacToe;
